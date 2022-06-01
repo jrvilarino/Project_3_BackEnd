7e34d2ea42505b04ebce3a7d99b456f5c4f7969a
Junior Vilarino <jrvilarino@gmail.com>
2022-06-01 15:27:57 -0300

    backend pronto

---
+++ routes/step.routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const Task = require("../models/Task.model");
+const Step = require("../models/Step.model");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
+//criar step
+router.post("/step", isAuthenticated, attachCurrentUser, async (req, res) => {
+  try {
+    const { taskid } = req.body;
+    const result = await Step.create({ ...req.body });
+    await Task.updateOne({ _id: taskid }, { $push: { steps: result._id } });
+    return res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: "Step creation failed." });
+  }
+});
+//atualizar estado do Step(feito ou não)
+router.patch(
+  "/step/:_id",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { _id } = req.params;
+
+      const result = await Step.findOneAndUpdate(
+        { _id },
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+
+      if (!result) {
+        return res.status(404).json({ msg: "Step not found." });
+      }
+
+      return res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ msg: "Action failed." });
+    }
+  }
+);
+//deletar step
+router.delete(
+  "/step/:_id",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { _id } = req.params;
+
+      const result = await Step.findOneAndDelete({ _id });
+
+      return res.status(200).json({});
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ msg: "Internal server error." });
+    }
+  }
+);
+
+module.exports = router;
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const Task = require("../models/Task.model");
 const User = require("../models/User.model");
+const Step = require("../models/Step.model");
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const attachCurrentUser = require("../middlewares/attachCurrentUser");
 
@@ -18,20 +19,58 @@ router.post("/task", isAuthenticated, attachCurrentUser, async (req, res) => {
     return res.status(500).json({ msg: "Task creation failed." });
   }
 });
-//get listar tarefas
-router.get(
-  "/task/:date",
+//get listar tarefas do dia
+router.get("/tasks/:date", isAuthenticated, async (req, res) => {
+  try {
+    const { date } = req.params;
+    const result = await Task.find({ date: { $eq: date } });
+
+    if (!result) {
+      return res
+        .status(404)
+        .json({ msg: "There is no tasks in that date yet" });
+    }
+
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: "Action failed." });
+  }
+});
+
+//get para detalhes da tarefa
+router.get("/task/:_id", isAuthenticated, async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const result = await Task.findOne({ _id }).populate("steps");
+
+    if (!result) {
+      return res.status(404).json({ msg: "Task not found." });
+    }
+
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: "Action failed." });
+  }
+});
+//patch editar tarefas
+router.patch(
+  "/task/:_id",
   isAuthenticated,
   attachCurrentUser,
   async (req, res) => {
     try {
-      const { date } = req.params;
-      const result = await Task.find({ date: { $eq: date } });
+      const { _id } = req.params;
+
+      const result = await Task.findOneAndUpdate(
+        { _id, createdBy: req.user._id },
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
 
       if (!result) {
-        return res
-          .status(404)
-          .json({ msg: "There is no tasks in that date yet" });
+        return res.status(404).json({ msg: "Task not found." });
       }
 
       return res.status(200).json(result);
@@ -41,8 +80,28 @@ router.get(
     }
   }
 );
-//get para detalhes da tarefa
-//patch editar tarefas
 //delete apagar tarefas
+router.delete(
+  "/task/:_id",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { _id } = req.params;
+
+      const result = await Task.findOneAndDelete({
+        _id,
+        createdBy: req.user._id,
+      });
+
+      await Step.deleteMany({ taskid: _id });
+
+      return res.status(200).json({});
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ msg: "Internal server error." });
+    }
+  }
+);
 
 module.exports = router;
